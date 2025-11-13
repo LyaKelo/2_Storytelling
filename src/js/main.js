3716d0ea76f5de9409bf5a8525cae46122743097
LyaKelo <lya.waeber@studentfr.ch>
2025-11-13 10:34:00 +0100

    13.11 - morning break - more content added

--- src/js/main.js
+++ src/js/main.js
@@ -14,6 +14,7 @@ const tl = gsap.timeline({
   },
 });
 
+// zoom 1
 tl.to(".zoom-01-start-button", {
   y: "100%",
   opacity: 0,
@@ -35,10 +36,187 @@ tl.to(".zoom-01-start-button", {
     duration: 1,
     ease: "none",
   })
-  .to(".zoom-02-element-1", { scale: 3, y: "100%", duration: 4, ease: "none" })
-  .to(".zoom-02-element-2", { scale: 3, y: "100%", duration: 4, ease: "none" })
+  // zoom 2
+  .to(".zoom-02-element-1", {
+    scale: 3,
+    opacity: 0,
+    y: "100%",
+    duration: 4,
+    ease: "none",
+  })
+  .to(".zoom-02-element-2", {
+    scale: 3,
+    opacity: 0,
+    y: "100%",
+    duration: 4,
+    ease: "none",
+  })
+  .to(".zoom-02-moon", {
+    scale: 3,
+    opacity: 0,
+    y: "100%",
+    duration: 4,
+    ease: "none",
+  })
+  .to(".zoom-02-text", {
+    scale: 3,
+    opacity: 0,
+    y: "100%",
+    duration: 4,
+    ease: "none",
+  })
   .to(".zoom-02-background", {
     scale: 3,
+    opacity: 0,
+    duration: 1,
+    ease: "none",
+  })
+  // zoom 3
+  .to(".zoom-03-element-1", {
+    scale: 3,
+    opacity: 0,
+    y: "100%",
+    duration: 4,
+    ease: "none",
+  })
+  .to(".zoom-03-element-2", {
+    scale: 3,
+    opacity: 0,
+    y: "100%",
+    duration: 4,
+    ease: "none",
+  })
+  .to(".zoom-03-element-3", {
+    scale: 3,
+    opacity: 0,
+    y: "100%",
+    duration: 4,
+    ease: "none",
+  })
+  .to(".zoom-03-element-4", {
+    scale: 3,
+    opacity: 0,
+    y: "100%",
+    duration: 4,
+    ease: "none",
+  })
+  .to(".zoom-03-element-5", {
+    scale: 3,
+    opacity: 0,
+    y: "100%",
+    duration: 4,
+    ease: "none",
+  })
+  .to(".zoom-03-element-6", {
+    scale: 3,
+    opacity: 0,
+    y: "100%",
+    duration: 4,
+    ease: "none",
+  })
+  .to(".zoom-03-element-7", {
+    scale: 3,
+    opacity: 0,
+    y: "100%",
+    duration: 4,
+    ease: "none",
+  })
+  .to(".zoom-03-element-8", {
+    scale: 3,
+    opacity: 0,
+    y: "100%",
+    duration: 4,
+    ease: "none",
+  })
+  .to(".zoom-03-text", {
+    scale: 3,
+    opacity: 0,
+    y: "100%",
+    duration: 4,
+    ease: "none",
+  })
+  .to(".zoom-03-text-2", {
+    scale: 3,
+    opacity: 0,
+    y: "100%",
+    duration: 4,
+    ease: "none",
+  })
+  .to(".zoom-03-background", {
+    scale: 3,
+    opacity: 0,
+    y: "100%",
+    duration: 4,
+    ease: "none",
+  })
+  // zoom 4
+  .to(".zoom-04-element-1", {
+    scale: 3,
+    opacity: 0,
+    y: "100%",
+    duration: 4,
+    ease: "none",
+  })
+  .to(".zoom-04-element-2", {
+    scale: 3,
+    opacity: 0,
+    y: "100%",
+    duration: 4,
+    ease: "none",
+  })
+  .to(".zoom-04-text", {
+    scale: 3,
+    opacity: 0,
+    y: "100%",
+    duration: 4,
+    ease: "none",
+  })
+  .to(".zoom-04-text-2", {
+    scale: 3,
+    opacity: 0,
+    y: "100%",
+    duration: 4,
+    ease: "none",
+  })
+  .to(".zoom-04-background", {
+    scale: 3,
+    opacity: 0,
+    y: "100%",
+    duration: 4,
+    ease: "none",
+  })
+  // zoom 5
+  .to(".zoom-05-element-1", {
+    scale: 3,
+    opacity: 0,
+    y: "100%",
+    duration: 4,
+    ease: "none",
+  })
+  .to(".zoom-05-element-2", {
+    scale: 3,
+    opacity: 0,
+    y: "100%",
+    duration: 4,
+    ease: "none",
+  })
+  .to(".zoom-05-text", {
+    scale: 3,
+    opacity: 0,
+    y: "100%",
+    duration: 4,
+    ease: "none",
+  })
+  .to(".zoom-05-text-2", {
+    scale: 3,
+    opacity: 0,
+    y: "100%",
+    duration: 4,
+    ease: "none",
+  })
+  .to(".zoom-05-background", {
+    scale: 3,
+    opacity: 0,
     y: "100%",
     duration: 4,
     ease: "none",
